0e187b59d0e120e6844c8abbb5dbcd4f5517fb86
towfeeqfayaz11 <towfeeqpandith@gmail.com>
2021-04-10 06:04:30 +0900

    working

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 db = SQLAlchemy(app)
 
-class ToDo(db.Model):
+class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow())
@@ -18,6 +18,8 @@ class ToDo(db.Model):
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
+        if task_content == '':
+            return redirect('/')
         new_task = Todo(content=task_content)
         try:
             db.session.add(new_task)
@@ -30,7 +32,33 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html',tasks=tasks)
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'there was a problem deleting that task'
 
+@app.route('/update/<int:id>',methods=['POST','GET'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'there was an issue updating your task'
+    else:
+        return render_template('update.html',task=task)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# python
+# from app import db
+# db.create_all()
+# exit()
